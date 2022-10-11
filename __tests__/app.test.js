347eff3c01144dbc776faa81e0277b7df96785b3
timm-d <timdunstan1@gmail.com>
2022-10-11 15:53:58 +0100

    adds testing for correct and incorrect paths

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -24,20 +24,19 @@ describe("GET/api/categories", () => {
       .get("/api/categories")
       .expect(200)
       .then(({ body }) => {
-        
-          const {categories} = body;
-          expect(categories.length).toBe(4);
-          expect(Array.isArray(categories)).toBe(true);
+        const { categories } = body;
+        expect(categories.length).toBe(4);
+        expect(Array.isArray(categories)).toBe(true);
 
-          categories.forEach(category => {
-            expect.objectContaining({
-              slug: expect.any(String),
-              description: expect.any(String),
-            });
+        categories.forEach((category) => {
+          expect.objectContaining({
+            slug: expect.any(String),
+            description: expect.any(String),
           });
+        });
       });
   });
-})
+});
 
 test("404: returns 'Not found' when url is incorrect", () => {
   return request(app)
@@ -46,4 +45,34 @@ test("404: returns 'Not found' when url is incorrect", () => {
     .then((response) => {
       expect(response.body).toEqual({ msg: "Not Found" });
     });
+});
+// TICKET 05
+
+describe("GET api/users", () => {
+  test("200: responds with an array of users objects", () => {
+    return request(app)
+      .get("/api/users")
+      .expect(200)
+      .then(({ body }) => {
+        const { users } = body;
+        expect(users.length).toBe(4);
+        expect(Array.isArray(users)).toBe(true);
+
+        users.forEach((user) => {
+          expect.objectContaining({
+            username: expect.any(String),
+            name: expect.any(String),
+            avatar_url: expect.any(String),
+          });
+        });
+      });
+  });
+  test("404: returns 'Not Found' when url is incorrect", () => {
+    return request(app)
+      .get("/api/nousersherethen")
+      .expect(404)
+      .then((response) => {
+        expect(response.body).toEqaul({ msg: "Not Found" });
+      });
+  });
 });
