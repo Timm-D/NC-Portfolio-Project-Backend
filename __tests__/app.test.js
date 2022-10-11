547e0596536edc5e61c5e986e3168efcc6726933
timm-d <timdunstan1@gmail.com>
2022-10-11 11:57:08 +0100

    adds server error handling to app and completes test suite for ticket 04

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -24,20 +24,19 @@ describe("GET/api/categories", () => {
       .get("/api/categories")
       .expect(200)
       .then(({ body }) => {
-        
-          const {categories} = body;
-          expect(categories.length).toBe(4);
-          expect(Array.isArray(categories)).toBe(true);
+        const { categories } = body;
+        expect(categories.length).toBe(4);
+        expect(Array.isArray(categories)).toBe(true);
 
-          categories.forEach(category => {
-            expect.objectContaining({
-              slug: expect.any(String),
-              description: expect.any(String),
-            });
+        categories.forEach((category) => {
+          expect.objectContaining({
+            slug: expect.any(String),
+            description: expect.any(String),
           });
+        });
       });
   });
-})
+});
 
 test("404: returns 'Not found' when url is incorrect", () => {
   return request(app)
@@ -46,4 +45,30 @@ test("404: returns 'Not found' when url is incorrect", () => {
     .then((response) => {
       expect(response.body).toEqual({ msg: "Not Found" });
     });
+});
+//TICKET 04
+describe("GET/api/reviews/:review_id", () => {
+  test("200: responds with an object containing the properties of a given review_id", () => {
+    return request(app)
+      .get("/api/reviews/13")
+      .expect(200)
+      .then(({ body }) => {
+        const { review } = body;
+        expect(review).toEqual(
+          expect.objectContaining({
+            review_id: 13,
+            title: "Settlers of Catan: Don't Settle For Less",
+            designer: "Klaus Teuber",
+            owner: "mallionaire",
+            review_img_url:
+              "https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg",
+            review_body:
+              "You have stumbled across an uncharted island rich in natural resources, but you are not alone; other adventurers have come ashore too, and the race to settle the island of Catan has begun! Whether you exert military force, build a road to rival the Great Wall, trade goods with ships from the outside world, or some combination of all three, the aim is the same: to dominate the island. Will you prevail? Proceed strategically, trade wisely, and may the odds be in favour.",
+            category: "social deduction",
+            created_at: new Date(788918400),
+            votes: 16,
+          })
+        );
+      });
+  });
 });
--- app.js
+++ app.js
@@ -10,4 +10,8 @@ app.all("/*", (req, res) => {
   res.status(404).send({ msg: "Not Found" });
 });
 
-module.exports = app;
+app.use((err, req, res, next) => {
+  res.status(500).send({msg: "Internal Server Error"})
+});
+
+module.exports = app
