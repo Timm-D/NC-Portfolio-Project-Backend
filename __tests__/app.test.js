87f37970c4796907817965a8774678fdd64e4ef1
timm-d <timdunstan1@gmail.com>
2022-10-12 11:03:50 +0100

    completed tests for 200

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -47,37 +47,6 @@ test("404: returns 'Not found' when url is incorrect", () => {
     });
 });
 
-// TICKET 05
-
-describe.only("GET api/users", () => {
-  test("200: responds with an array of users objects", () => {
-    return request(app)
-      .get("/api/users")
-      .expect(200)
-      .then(({ body }) => {
-        const { users } = body;
-        expect(users.length).toBe(4);
-        expect(Array.isArray(users)).toBe(true);
-
-        users.forEach((user) => {
-          expect.objectContaining({
-            username: expect.any(String),
-            name: expect.any(String),
-            avatar_url: expect.any(String),
-          });
-        });
-      });
-  });
-  test("404: returns 'Not Found' when url is incorrect", () => {
-    return request(app)
-      .get("/api/nousersherethen")
-      .expect(404)
-      .then((response) => {
-        expect(response.body).toEqual({ msg: "Not Found" });
-      });
-  });
-});
-
 //TICKET 04
 describe("GET/api/reviews/:review_id", () => {
   test("200: responds with an object containing the properties of a given review_id", () => {
@@ -117,6 +86,68 @@ describe("GET/api/reviews/:review_id", () => {
       .expect(404)
       .then((response) => {
         expect(response.body).toEqual({ msg: "Not Found" });
+      });
+  });
+});
+
+// TICKET 05
+
+describe.only("GET api/users", () => {
+  test("200: responds with an array of users objects", () => {
+    return request(app)
+      .get("/api/users")
+      .expect(200)
+      .then(({ body }) => {
+        const { users } = body;
+        expect(users.length).toBe(4);
+        expect(Array.isArray(users)).toBe(true);
+
+        users.forEach((user) => {
+          expect.objectContaining({
+            username: expect.any(String),
+            name: expect.any(String),
+            avatar_url: expect.any(String),
+          });
+        });
+      });
+  });
+  test("404: returns 'Not Found' when url is incorrect", () => {
+    return request(app)
+      .get("/api/nousersherethen")
+      .expect(404)
+      .then((response) => {
+        expect(response.body).toEqual({ msg: "Not Found" });
+      });
+  });
+});
+
+//TICKET 06
+
+describe.only("PATCH api/reviews/:review_id", () => {
+  test("200: responds with review object updated to reflect impact of newVote", () => {
+    let requestObject = { inc_votes: 10 };
+
+    return request(app)
+      .patch("/api/reviews/13")
+      .send(requestObject)
+      .expect(200)
+      .then(({ body }) => {
+        const { review } = body;
+        expect(review).toEqual(
+          expect.objectContaining({
+            review_id: 13,
+            title: "Settlers of Catan: Don't Settle For Less",
+            designer: "Klaus Teuber",
+            owner: "mallionaire",
+            review_img_url:
+              "https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg",
+            review_body:
+              "You have stumbled across an uncharted island rich in natural resources, but you are not alone; other adventurers have come ashore too, and the race to settle the island of Catan has begun! Whether you exert military force, build a road to rival the Great Wall, trade goods with ships from the outside world, or some combination of all three, the aim is the same: to dominate the island. Will you prevail? Proceed strategically, trade wisely, and may the odds be in favour.",
+            category: "social deduction",
+            created_at: expect.any(String),
+            votes: 26,
+          })
+        );
       });
   });
 });
