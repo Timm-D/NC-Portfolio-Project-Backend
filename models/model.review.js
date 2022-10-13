04406a29739199fdb0b75f4bba84755072aca77a
timm-d <timdunstan1@gmail.com>
2022-10-13 14:23:29 +0100

    add comment_count prop to review response Obj

--- models/model.review.js
+++ models/model.review.js
@@ -4,9 +4,12 @@ const format = require("pg-format");
 exports.fetchReviewById = (review_id) => {
   return db
     .query(
-      `SELECT * 
-    FROM reviews
-    WHERE review_id = $1`,
+      `SELECT reviews. *, COUNT(comment_id) ::INT AS comment_count 
+      FROM reviews 
+      LEFT JOIN comments
+      ON reviews.review_id = comments.review_id
+      WHERE reviews.review_id = $1
+      GROUP BY reviews.review_id;`,
       [review_id]
     )
     .then((result) => {
