f007f97bf1558c0a20f2703b06e5b2d39d143fff
timm-d <timdunstan1@gmail.com>
2022-10-14 15:12:06 +0100

    heroku setup 2

--- app.js
+++ app.js
@@ -54,4 +54,5 @@ app.use((err, req, res, next) => {
   res.status(500).send({msg: "Internal Server Error"})
 });
 
+
 module.exports = app
---
+++ listen.js
@@ -0,0 +1,8 @@
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`Listening on ${PORT}...`);
+  });
+
+
